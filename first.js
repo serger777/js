0582c25224e391f3a9d73b1836da186c7156a350
sergey.gerasimov <sergey.gerasimov@realize-it.ru>
2019-01-24 09:27:11 +0300

    5

--- first.js
+++ first.js
@@ -1,69 +1,62 @@
 // task 1
-// function ArrayBack(main) {
-//     var inter = function (str) {
-//         if (str != 0) {
-//             result = -str;
-//         } else {
-//             result = str;
-//         }
-//         return result;
-//     }
-//     result = main.map(inter);
-//     return result;
-// }
-// var a = [-10, 0, 10, -4, 5, -6, 5, -7, 8, -9];
-// var result = ArrayBack(a);
+function ArrayBack(main) {
+    var inter = function (str) {
+        if (str != 0) {
+            result = -str;
+        } else {
+            result = str;
+        }
+        return result;
+    }
+    result = main.map(inter);
+    return result;
+}
+var a = [-10, 0, 10, -4, 5, -6, 5, -7, 8, -9];
+var result = ArrayBack(a);
 
 
 // // task 2
-// function day(name) {
-//     if (name != ""){
-//         var name = name;
-//     }else{
-//         var name = "people";
-//     }
-//     var time = new Date().getHours();
-//     if (time > 6 && time < 10) {
-//         alert("Good morning " + name);
-//     }
-//     if (time > 10 && time < 15) {
-//         alert("Good morning" + name);
-//     }
-//     if (time > 15 && time < 21) {
-//         alert("Good evening " + name);
-//     }
-//     if (time > 21 && time < 6){
-//         alert("Good night" + name);
-//     }
-// }
-//
-// var name = prompt("you name");
-// day(name);
-//
-//
-// //task3
-// function PriceMask(number) {
-//     var a = String(number);
-//     var price = a.split("");
-//     var lengt = price.length;
-//     if (lengt > 5) {
-//         price.splice(lengt - 5, 0, "  ")
-//     }
-//     if (lengt > 2) {
-//         price.splice(lengt - 1, 0, " , ");
-//     }
-//
-//     price.push(" ")
-//     price.push("rub")
-//     var price_f = price.join("");
-//
-// }
-//
-// PriceMask(121348);
-//
-//
-//
-// //task 4
+function day(name) {
+    if (name != ""){
+        var name = name;
+    }else{
+        var name = "people";
+    }
+    var time = new Date().getHours();
+    if (time > 6 && time < 10) {
+        alert("Good morning " + name);
+    }
+    if (time > 10 && time < 15) {
+        alert("Good morning" + name);
+    }
+    if (time > 15 && time < 21) {
+        alert("Good evening " + name);
+    }
+    if (time > 21 && time < 6){
+        alert("Good night" + name);
+    }
+}
+
+var name = prompt("you name");
+day(name);
+
+
+ //task3
+function PriceMask(number) {
+    var price = String(number);
+    var lengt = price.length;
+    if (lengt > 2 && lengt < 4 ) {
+       var price = price.substr(0, lengt-2)+ "," + price.substr(lengt-2)  + "rub"
+    }
+    if (lengt > 5) {
+        var price =  price.substr(0, lengt-5)+ " " + price.substr(lengt-5, lengt-2)+ "," + price.substr(lengt-2)  + "rub"
+    }
+    console.log(price);
+}
+
+PriceMask(121348);
+
+//task 4
 function whatDay() {
     flag = true;
     arDay = ["monday",
