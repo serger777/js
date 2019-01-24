7438ce7b557c4d4e3a0c5bc3963791f51fb3634b
sergey.gerasimov <sergey.gerasimov@realize-it.ru>
2019-01-24 16:00:31 +0300

    1

--- first.js
+++ first.js
@@ -1,26 +1,20 @@
 // task 1
 function ArrayBack(main) {
-    var inter = function (str) {
-        if (str != 0) {
-            result = -str;
-        } else {
-            result = str;
-        }
-        return result;
-    }
-    result = main.map(inter);
-    return result;
+ var charMain =   main.map(function (str) {
+     return   (str) ? -str : str;
+    });
+    console.log(charMain);
 }
 var a = [-10, 0, 10, -4, 5, -6, 5, -7, 8, -9];
-var result = ArrayBack(a);
+ArrayBack(a);
 
 
 // // task 2
 function day(name) {
-    if (name !== ""){
+    if (name) {
         var name = name;
-    }else{
-        var name = "people";
+    } else {
+         name = "people";
     }
     var time = new Date().getHours();
     if (time > 6 && time < 10) {
@@ -32,30 +26,12 @@ function day(name) {
     if (time > 15 && time < 21) {
         alert("Good evening " + name);
     }
-    if (time > 21 && time < 6){
+    if (time > 21 && time < 6) {
         alert("Good night" + name);
     }
 }
-
 var name = prompt("you name");
 day(name);
-
-
- //task3
-function PriceMask(number) {
-    var price = String(number);
-    var lengt = price.length;
-    if (lengt > 2 && lengt < 4 ) {
-       var price = price.substr(0, lengt-2)+ "," + price.substr(lengt-2)  + "rub"
-    }
-    if (lengt > 5) {
-        var price =  price.substr(0, lengt-5)+ " " + price.substr(lengt-5, lengt-2)+ "," + price.substr(lengt-2)  + "rub"
-    }
-    console.log(price);
-}
-
-PriceMask(121348);
-
 //task 4
 function whatDay() {
     flag = true;
@@ -65,7 +41,8 @@ function whatDay() {
         "thursday",
         "friday",
         "saturday",
-        "sunday",]
+        "sunday",];
+
     function text(text) {
         var div = document.createElement('div');
         div.className = 'data';
@@ -74,7 +51,7 @@ function whatDay() {
     }
     function numberDay(a) {
         var dayName = arDay[a - 1];
-        if (dayName != undefined) {
+        if (dayName) {
             text(dayName)
             flag = false;
         } else {
@@ -88,3 +65,32 @@ function whatDay() {
 }
 
 whatDay();
+
+
+
+
+//task 3
+function numMask(number) {
+    console.log(new Intl.NumberFormat('ru-RU',{
+        style: "currency",
+        currency : "RUB"
+    }).format(number));
+}
+
+var number = 1234523216545;
+numMask(number);
+
+//task3
+// function PriceMask(number) {
+//     var price = String(number);
+//     var lengt = price.length;
+//     if (lengt > 2 && lengt < 4) {
+//         var price = price.substr(0, lengt - 2) + "," + price.substr(lengt - 2) + "rub"
+//     }
+//     if (lengt > 5) {
+//         var price = price.substr(0, lengt - 5) + " " + price.substr(lengt - 5, lengt - 2) + "," + price.substr(lengt - 2) + "rub"
+//     }
+//     console.log(price);
+// }
+//
+// PriceMask(121348);
